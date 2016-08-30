64c50b5eb1cd815e9e443aee1cd8660959af9158
Philippe Massicotte <pmassicotte@hotmail.com>
2016-08-30 10:40:50 +0200

    Update the book

---
+++ _deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+# clone the repository to the book-output directory
+git clone -b gh-pages \
+  https://${GITHUB_PAT}@github.com/${TRAVIS_REPO_SLUG}.git \
+  book-output
+
+cd book-output
+cp -r ../_book/* ./
+git add --all *
+git commit -m"Update the book"
+git push origin gh-pages
